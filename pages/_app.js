d84410139f29d79f54ff76a107b9840abdbfd5d3
kmocorro <kevinmocorro.github@gmail.com>
2020-06-02 08:49:42 +0800

    aaa

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ export default function MyApp(props) {
   return (
     <React.Fragment>
       <Head>
-        <title>CoDECS - Fab4 I/O </title>
+        <title>CoDECS Logout - Fab4 I/O </title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
