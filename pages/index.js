cb738c884f34adad20e702409906e4df041bafe7
kmocorro <kevinmocorro.github@gmail.com>
2020-06-03 10:06:18 +0800

    aaa

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,11 @@ import { withAuthSync, logout } from '../utils/auth';
 import nextCookie from 'next-cookies';
 
 
-function Index() {
+function Index(props) {
+
+  // state for login profile
+  const [ loginProfile, setLoginProfile ] = useState('');
+  console.log(loginProfile.username);
 
   // state for Employee Number
   const [ employee_number, setEmployee_number ] = useState('');
@@ -30,6 +34,28 @@ function Index() {
 
   // state for new scan remove image
   const [ scan, setScan ] = useState(true);
+
+  // post/get login data
+  useEffect(() => {
+    async function fetchLoginInfo(){
+      let routePOST = 'http://dev-metaspf401.sunpowercorp.com:4000/getuserprofile'
+      
+      let responsePOST = await fetch(`${routePOST}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          token: props.token
+        })
+      })
+
+      if(responsePOST.status === 200){
+        setLoginProfile(await responsePOST.json());
+      }
+
+    }
+
+    fetchLoginInfo();
+  }, []);
   
   // get user data info
   useEffect(() => {
@@ -57,7 +83,7 @@ function Index() {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-              //token: props.token,
+              token: props.token,
               employeeNumber: userData.id,
               mode: 'OUT',
               device: 'EXIT1',
@@ -76,7 +102,7 @@ function Index() {
             async function fetchAttendance(){
               let route = 'http://dev-metaspf401.sunpowercorp.com:4000/getattendancelogs'
               
-              let response = await fetch(`${route}/OUT/EXIT1`)
+              let response = await fetch(`${route}/OUT/${loginProfile.username}`)
           
               if(response.status === 200){
                 setRecentLogs(await response.json())
@@ -97,7 +123,7 @@ function Index() {
     async function fetchAttendance(){
       let route = 'http://dev-metaspf401.sunpowercorp.com:4000/getattendancelogs'
       
-      let response = await fetch(`${route}/OUT/EXIT1`)
+      let response = await fetch(`${route}/OUT/${loginProfile.username}`)
   
       if(response.status === 200){
         setRecentLogs(await response.json())
